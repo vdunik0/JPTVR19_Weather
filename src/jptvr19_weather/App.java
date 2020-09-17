8edff95259ca4dfe8866cf4746e17beb6599eece
pupil <pupil@ivkhk.ee>
2020-09-17 11:21:19 +0300

    commit

--- src/jptvr19_weather/App.java
+++ src/jptvr19_weather/App.java
@@ -7,6 +7,7 @@ package jptvr19_weather;
 
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -17,6 +18,16 @@ class App {
         System.out.println("---Weather---");
         System.out.println("Задание. Создаем зубчатый массив");
         int [][] tempInYear = new int[12][];
+        System.out.print("     ");
+        for (int i = 1; i < 32; i++) {
+            System.out.printf("%4d",i);
+        }
+        System.out.println();
+        for (int i = 1; i < 32; i++) {
+            System.out.print("----");
+        }
+        System.out.println();
+
         int max = 30;
         int min = -30;
         Random random = new Random();
@@ -37,13 +48,19 @@ class App {
                 case 11: min = -30; max = -10; n = 31; break;
             }
             tempInYear[i] = new int[n];
+            System.out.printf("%4d) ",i+1);
             for (int j = 0; j< tempInYear[i].length;j++) {
                 tempInYear[i][j] = random.nextInt(max-min+1)+min;
                 System.out.printf("%4d",tempInYear[i][j]);
             }
             System.out.println();
         }   
-       
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Номер месяца");
+        int month = scanner.nextInt();
+        System.out.println("Denj mesjaca");
+        int day = scanner.nextInt();
+        System.out.printf("В этот день температура былo %4d градусов%n", tempInYear[month-1][day-1]);
     }
     
 }
